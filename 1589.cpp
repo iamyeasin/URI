868b8ba851a7ba62b78ec6bf2ee76799f6dc0ac8
yeasin Rahaman <ryeashin@gmail.com>
2017-07-03 18:21:23 +0600

    Create 1589.cpp

---
+++ 1589.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define sf scanf 
+#define pf printf
+
+using namespace std;
+
+int main()
+{
+	int a,r1,r2;
+	sf("%d",&a);
+	while(a--)
+	{
+		sf("%d %d",&r1,&r2);
+		pf("%d\n",r1+r2);
+	}
+	
+	
+	return 0;
+}
+
