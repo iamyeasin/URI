31228d4a30fde7edae5e5ebb5f0d8bd84e743b41
yeasin Rahaman <ryeashin@gmail.com>
2017-07-03 18:32:21 +0600

    Create 1759.cpp

---
+++ 1759.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#define sf scanf 
+#define pf printf
+
+int main()
+{
+	int a;
+	sf("%d",&a);
+	for(int i=1; i<a; i++)
+	{
+		pf("Ho ");
+	}
+	pf("Ho!\n");
+	
+	
+	
+	
+	return 0;
+}
+
