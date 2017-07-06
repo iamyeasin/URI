90b9883cf09552e58b672f7db2a0db0119c5f2d4
yeasin Rahaman <ryeashin@gmail.com>
2017-07-06 10:13:14 +0600

    Create 1957.cpp

---
+++ 1957.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define sf scanf
+#define pf printf
+
+using namespace std;
+
+int main()
+{
+    long long n1,n2,n3,n4;
+    sf("%lld",&n1);
+
+    pf("%X\n",n1);
+
+
+
+    return 0;
+}
