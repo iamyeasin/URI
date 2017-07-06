bea297ce7e20fa8f1a9cd3db4e784f2bd3815bc0
yeasin Rahaman <ryeashin@gmail.com>
2017-07-06 08:53:24 +0600

    Create 1924.cpp

---
+++ 1924.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define sf scanf
+#define pf printf
+
+using namespace std;
+
+int main()
+{
+    int kase;
+    char name[1024];
+    sf("%d\n",&kase);
+
+    for(int i=1; i<=kase; i++)
+    {
+        gets(name);
+    }
+
+    puts("Ciencia da Computacao");
+
+    return 0;
+}
