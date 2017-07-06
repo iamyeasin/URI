5997bc93ff995cd7674bd2de961475071c9be10a
yeasin Rahaman <ryeashin@gmail.com>
2017-07-06 09:56:13 +0600

    Create 1930.cpp

---
+++ 1930.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define sf scanf
+#define pf printf
+
+using namespace std;
+
+int main()
+{
+    int n1,n2,n3,n4;
+    sf("%d %d %d %d",&n1,&n2,&n3,&n4);
+
+    pf("%d\n",(n1+n2+n3+n4-3));
+
+    return 0;
+}
