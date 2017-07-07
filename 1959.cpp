b13633a5fada8c6a6448dc06abbec00d47924249
yeasin Rahaman <ryeashin@gmail.com>
2017-07-07 14:51:30 +0600

    Create 1959.cpp

---
+++ 1959.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define pf printf
+#define sf scanf
+
+using namespace std;
+
+int main()
+{
+    unsigned long long int x,y;
+    sf("%llu %llu",&x,&y);
+
+
+    pf("%llu\n",x*y);
+
+    return 0;
+}
