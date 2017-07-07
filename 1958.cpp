d9848d9c33ad1e29e0246702e6b682ca5f23e207
yeasin Rahaman <ryeashin@gmail.com>
2017-07-07 14:01:53 +0600

    Create 1958.cpp

---
+++ 1958.cpp
@@ -0,0 +1,29 @@
+//That proves that I am the biggest idiot
+
+
+#include<bits/stdc++.h>
+#define pf printf
+#define sf scanf
+
+using namespace std;
+
+int main()
+{
+//    while(1)
+
+        double x;
+        scanf("%lf",&x);
+
+        if(x == 0)
+        {
+            pf("%+0.4E\n",x);
+        }
+        else
+        {
+            pf("%+.4E\n",x);
+        }
+
+
+
+    return 0;
+}
