c348009de401c202349db8fed510960f7dfbf823
yeasin Rahaman <ryeashin@gmail.com>
2017-07-10 03:01:46 +0600

    Create 2581.cpp

---
+++ 2581.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define sf scanf
+#define pf printf
+#define pi 3.14
+
+using namespace std;
+
+
+int main()
+{
+    int n;
+    string s;
+    sf("%d\n",&n);
+
+    for(int i=1; i<=n; i++)
+    {
+        getline(cin,s);
+        puts("I am Toorg!");
+    }
+
+    return 0;
+}
