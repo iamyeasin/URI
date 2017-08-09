16160189258bffdc64f85471e4deb7a007a86084
yeasin Rahaman <ryeashin@gmail.com>
2017-08-09 10:08:19 +0600

    Update 1471.cpp

--- 1471.cpp
+++ 1471.cpp
@@ -4,6 +4,43 @@
 
 using namespace std;
 
+bool ret[100004];
+
+int main(){
+    //    freopen("input.txt","rt",stdin);
+    //    freopen("output.txt","wt",stdout);
+   
+    int a,b;
+    while(scanf("%d %d",&a,&b) != EOF){
+        for(int i=1; i<=b; i++){
+            long n; cin >> n;
+            ret[n] = 1;
+        }
+        int fl=0,counter=0; // to check everyone is alive
+        for(int i=1; i<=a; i++){
+            if(!ret[i]){
+                counter++;
+                if(counter== (a-b))printf("%d \n",i);
+                else printf("%d ",i);
+                fl = true;
+            }
+        }
+        if(!fl)printf("*\n");
+        memset(ret,0,sizeof(ret));
+    }
+
+    return 0;
+}
+
+
+
+/*
+#include<bits/stdc++.h>
+#define sf scanf
+#define pf printf
+
+using namespace std;
+
 bool ret[100004]; int nret[100004];
 
 int main(){
@@ -40,3 +77,4 @@ int main(){
 
     return 0;
 }
+*/
