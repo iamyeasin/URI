18bba574d9eba9ee61dd315d372f714aae9826fa
yeasin Rahaman <ryeashin@gmail.com>
2017-08-14 22:18:14 +0600

    Update 1495.cpp

--- 1495.cpp
+++ 1495.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// will sort based on differnce priority
 bool sortinrev(const pair<int,int> &a, const pair<int,int> &b){
     return(abs(a.first - a.second) < abs(b.first-b.second) );
 }
@@ -20,25 +21,26 @@ int main(){
         for(int i=1; i<=n; i++){
             sf("%ld%ld",&s,&r);
             if(s == r){
-                sz1++;
+                sz1++; // no need to collect the same values. because it'll take only one goal to max the points. so counting the same
             }
             else{
-                nor.push_back(make_pair(s,r));
+                nor.push_back(make_pair(s,r)); //collecting the difference to the different vector pair
             }
         }
 
         int sz2 = nor.size();
-
+        
         if(g >= sz1){
-            points += (sz1*3);
+            points += (sz1*3); 
             g -= sz1;
         }else if(g < sz1){
-            points += (g*3)+ (sz1-g);
+            points += (g*3)+ (sz1-g); // if thereis less goal i can buy. I also have to add the goals that were giving me draw points
             g -= sz1;
         }
 
         sort(nor.begin(), nor.end(), sortinrev);
 
+        // else manupulating the differences pair based on the priority which will take less goals to max the points
         if(sz2 >=0){
           for(int i=0; i<sz2; i++){
             long s,r;
