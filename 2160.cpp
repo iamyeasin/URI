7b8ae74e6fa0c145c33c7685a8a9efadb2031eeb
yeasin Rahaman <ryeashin@gmail.com>
2017-09-07 18:00:38 +0600

    Create 2160.cpp

---
+++ 2160.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+//    freopen("in.txt","rt",stdin);
+//    freopen("out.txt","wt",stdout);
+
+    int a=0,b;
+    string s;
+
+    getline(cin,s);
+    if(s.size() > 80) puts("NO");
+    else puts("YES");
+
+
+    return 0;
+}
