67f6e987a25e1efb9c50b8c3d7cb22e9b840cbd5
yeasin Rahaman <ryeashin@gmail.com>
2017-09-07 16:49:38 +0600

    Create 2146.cpp

---
+++ 2146.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define mx 1000007
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    while(scanf("%d",&a) != EOF){
+        printf("%d\n",a-1);
+    }
+
+
+    return 0;
+}
